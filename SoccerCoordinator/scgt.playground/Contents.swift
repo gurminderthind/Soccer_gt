41665cdd90f1d87c11b8081f968182cf9c58c359
gurminderthind <thindgurminder_s@hotmail.com>
2017-04-12 17:54:30 +0530

    Resubmission

--- SoccerCoordinator/scgt.playground/Contents.swift
+++ SoccerCoordinator/scgt.playground/Contents.swift
@@ -1,139 +1,79 @@
 //: Playground - noun: a place where people can play
 
-let playerName : [Int: String] = [
-    1: "Joe Smith",
-    2: "Jill Tanner",
-    3: "Bill Bon",
-    4: "Eva Gordon",
-    5: "Matt Gill",
-    6: "Kimmy Stein",
-    7: "Sammy Adams",
-    8: "Karl Saygan",
-    9: "Suzane Greenberg",
-    10: "Sal Dali",
-    11: "Joe Kavalier",
-    12: "Ben Finkelstein",
-    13: "Diego Soto",
-    14: "Chloe Alaska",
-    15: "Arnold Wilis",
-    16: "Phillip Clay",
-    17: "Les Clay",
-    18: "Herschel Krustofski"
-]
+let joeSmith: [String: Any] = ["Name": "Joe Smith", "Height": 42, "Experience": true, "Guardian": "Jim and Jan Smith"]
+let jillTanner: [String: Any] = ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardian": "Clara Tanner"]
+let billBon: [String: Any] = ["Name": "Bill Bon", "Height": 43, "Experience": true, "Guardian": "Sara and Jenny Bon"]
+let evaGordon: [String: Any] = ["Name": "Eva Gordon", "Height": 45, "Experience": false, "Guardian": "Wendy and Mike Gordon"]
+let mattGill: [String: Any] = ["Name": "Matt Gill", "Height": 40, "Experience": false, "Guardian": "Charles and Sylvia Gill"]
+let kimmyStein: [String: Any] = ["Name": "Kimmy Stein", "Height": 41, "Experience": false, "Guardian": "Bill and Hillary Stein"]
+let sammyAdams: [String: Any] = ["Name": "Sammy Adams", "Height": 45, "Experience": false, "Guardian": "Jeff Adams"]
+let karlSaygan: [String: Any] = ["Name": "Karl Saygan", "Height": 42, "Experience": true, "Guardian": "Heather Bledsoe"]
+let suzaneGreenberg: [String: Any] = ["Name": "Suzane Greenberg", "Height": 44, "Experience": true, "Guardian": "Henrietta Dumas"]
+let salDali: [String: Any] = ["Name": "Sal Dali", "Height": 41, "Experience": false, "Guardian": "Gala Dali"]
+let joeKavalier: [String: Any] = ["Name": "Joe Kavalier", "Height": 39, "Experience": false, "Guardian": "Sam and Elaine Kavalier"]
+let benFinkelstein: [String: Any] = ["Name": "Ben Finkelstein", "Height": 44, "Experience": false, "Guardian": "Aaron and Jill Finkelstein"]
+let diegoSoto: [String: Any] = ["Name": "Diego Soto", "Height": 41, "Experience": true, "Guardian": "Robin and Sarika Soto"]
+let chloeAlaska: [String: Any] = ["Name": "Chloe Alaska", "Height": 47, "Experience": false, "Guardian": "David and Jamie Alaska"]
+let arnoldWillis: [String: Any] = ["Name": "Arnold Willis", "Height": 43, "Experience": false, "Guardian": "Claire Willis"]
+let phillipHelm: [String: Any] = ["Name": "Phillip Helm", "Height": 44, "Experience": true, "Guardian": "Thomas Helm and Eva Jones"]
+let lesClay: [String: Any] = ["Name": "Les Clay", "Height": 42, "Experience": true, "Guardian": "Wynonna Brown"]
+let herschelKrustofski: [String: Any] = ["Name": "Herschel Krustofski", "Height": 45, "Experience": true, "Guardian": "Hyman and Rachel Krustofski"]
 
-let playerHeight : [Int: Int] = [
-    1: 42,
-    2: 36,
-    3: 43,
-    4: 45,
-    5: 40,
-    6: 41,
-    7: 45,
-    8: 42,
-    9: 44,
-    10: 41,
-    11: 39,
-    12: 44,
-    13: 41,
-    14: 47,
-    15: 43,
-    16: 44,
-    17: 42,
-    18: 45
-]
+let players = [joeSmith, jillTanner, billBon, evaGordon, mattGill, kimmyStein, sammyAdams, karlSaygan, suzaneGreenberg, salDali, joeKavalier, benFinkelstein, diegoSoto, chloeAlaska, arnoldWillis, phillipHelm, lesClay, herschelKrustofski]
 
-let playerSoccerExp : [Int: Bool] = [
-    1: true,
-    2: true,
-    3: true,
-    4: false,
-    5: false,
-    6: false,
-    7: false,
-    8: true,
-    9: true,
-    10: false,
-    11: false,
-    12: false,
-    13: true,
-    14: false,
-    15: false,
-    16: true,
-    17: true,
-    18: true
-]
+var experiencedPlayers: [[String: Any]] = []
+var unexperiencedPlayers: [[String: Any]] = []
 
-let playerGuardianName : [Int: String] = [
-    1: "Jim and Jan Smith",
-    2: "Clara Tanner",
-    3: "Sara and Jenny Bon",
-    4: "Wendy and Mike Gordon",
-    5: "Charles and Sylvia Gill",
-    6: "Bill and Hillary Stein",
-    7: "Jeff Adams",
-    8: "Heather Bledsoe",
-    9: "Henriette Dumas",
-    10: "Gala Dali",
-    11: "Sam and Elaine Kavalier",
-    12: "Aaron and Jill Finkelstein",
-    13: "Robin and Sarika Soto",
-    14: "David and Jamie Alaska",
-    15: "Claire Willis",
-    16: "Thomas Helm and Eva Jones",
-    17: "Wynonna Brown",
-    18: "Hyman and Rachel Krustofski"
-]
-
-var experiencedPlayers: [Int] = []
-var nonExperiencedPlayers: [Int] = []
-
-var teamDragons : [Int] = []
-var teamSharks : [Int] = []
-var teamRaptors : [Int] = []
-
-let totalPlayerInTeam = playerSoccerExp.count/3
-
-var numExpPlayers = 0
-
-for (index,experience) in playerSoccerExp {
-    if(experience == true){
-        numExpPlayers += 1
+for player in players {
+        if player ["Experience"] as! Bool == true {
+                experiencedPlayers.append(player)
+            } else {
+                unexperiencedPlayers.append(player)
+            }
     }
-}
 
-let expPlayersPerTeam = numExpPlayers/3
+var teamSharks: [[String: Any]] = []
+var teamDragons: [[String: Any]] = []
+var teamRaptors: [[String: Any]] = []
 
-for(index,experience) in playerSoccerExp{
-    if experience == true {
-        experiencedPlayers.append(index)
-    }else{
-        nonExperiencedPlayers.append(index)
+for i in 0..<experiencedPlayers.count {
+        let players = experiencedPlayers [i]
+        let mod = i % 3
+        if mod == 0 {
+                teamSharks.append(players)
+            }
+       else if mod == 1 {
+                teamDragons.append(players)
+            }
+        else {
+                teamRaptors.append(players)
+            }
     }
-}
 
-for i in 0..<expPlayersPerTeam {
-    
-    teamDragons.append(experiencedPlayers[i])
-    teamSharks.append(experiencedPlayers[i])
-    teamRaptors.append(experiencedPlayers[i])
-    
-}
+for i in 0..<unexperiencedPlayers.count {
+        let players = unexperiencedPlayers[i]
+        let mod = i % 3
+        if mod == 0 {
+                teamRaptors.append(players)
+            }
+        else if mod == 1 {
+                teamDragons.append(players)
+            }
+        else {
+                teamSharks.append(players)
+            }
+    }
 
-for i1 in expPlayersPerTeam..<totalPlayerInTeam{
-    teamDragons.append(nonExperiencedPlayers[i1])
-    teamSharks.append(nonExperiencedPlayers[i1])
-    teamRaptors.append(nonExperiencedPlayers[i1])
-}
 
-for playerIndex in teamDragons{
-    print("To: \(playerGuardianName[playerIndex]), Your child \(playerName[playerIndex]) from Team Dragon will attend their first team practice on April 30,2017")
-}
+for players in teamSharks {
+        print("Congrats \(players["Guardian"]!), your child, \(players["Name"]!), has been chosen to play on team Sharks. The first practice for the Sharks will be held on April 30th at 1 pm.")
+    }
 
-for playerIndex in teamSharks{
-    print("To: \(playerGuardianName[playerIndex]), Your child \(playerName[playerIndex]) from Team Sharks will attend their first team practice on April 30,2017")
-}
+for players in teamDragons {
+        print("Congrats \(players["Guardian"]!), your child, \(players["Name"]!), has been chosen to play on team Dragons. The first practice for the Dragons will be held on April 30th at 1 pm.")
+    }
 
-for playerIndex in teamRaptors{
-    print("To: \(playerGuardianName[playerIndex]), Your child \(playerName[playerIndex]) from Team Raptors will attend their first team practice on April 30,2017")
-}
+for players in teamRaptors {
+        print("Congrats \(players["Guardian"]!), your child, \(players["Name"]!), has been chosen to play on team Raptors. The first practice for the Raptors will be held on April 30th at 1 pm.")
+    }
 
